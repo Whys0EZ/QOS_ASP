0ae2d9c3b4ed1a5f809048427c21cb9a10b100f9
WhyS0EZ <luongvanchuong011094@gmail.com>
2025-08-25 17:31:12 +0700

    QOS Asp NET

--- obj/Debug/net8.0/QOS.AssemblyInfo.cs
+++ obj/Debug/net8.0/QOS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92774d037145a2164e5a7c9e0f66c6b09c778ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("QOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
